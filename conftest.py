566e332ed5d664e7d60c8f499d091bf429a4ed36
Marco Neiva <marcot.neiva@gmail.com>
2025-07-07 15:11:09 -0300

    conftest added

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture(scope="function")
+def driver():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.binary_location = "/usr/bin/chromium"  # Important for Docker!
+
+    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=options)
+    yield driver
+    driver.quit()
